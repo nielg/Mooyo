c05302520dab39a7108a6f52505981b69ec803ad
Niel Geysen <niel.geysen@student.ucll.be>
2026-04-01 16:08:27 +0200

    add base url Mooyo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nielg.github.io",
-  base: "Moyoo",
+  base: "Mooyo",
   i18n: {
     defaultLocale: "nl",
     locales: ["nl", "en"],
