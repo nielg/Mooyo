9e0d29e0292f7f6338fe49dc9093bb6e585f6c21
Niel Geysen <niel.geysen@student.ucll.be>
2026-04-06 12:56:44 +0200

    remvoe base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nielg.github.io",
-  base: "/Mooyo",
 
   i18n: {
     defaultLocale: "nl",
